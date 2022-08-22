29bef2e994165cdf3f7c2a87b46cd023dc3d4bb1
JamesNg <njames15@gmail.com>
2022-08-22 11:39:20 -0400

    commit tree 7b99bbb5eeb4ce26962733287ac25e9ebde0f722

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -7,8 +7,38 @@ class LinkedList
     @head = nil
   end
 
+  
   def nth_from_end(n)
-    # your code here
+    list_length = 0
+    node = head
+    while node
+      list_length += 1
+      node = node.next_node
+    end 
+
+    # opposite of while loop
+    # until node.nil?
+    #   node = node.next_node
+    #   list_length += 1
+    # end
+    
+    # edge cases where n = 0 or n > length of list
+    return nil if n < 1 || n > list_length
+
+    # example [4,3,2,1] -> list_length = 4
+    # if n = 1, then list_length - n + 1 = 3
+    node = head
+    i = 1
+    while i < list_length - n + 1
+      # traverses through linked list
+      node = node.next_node
+      i += 1
+    end
+
+    node.value
+   
   end
+
+
 
 end
